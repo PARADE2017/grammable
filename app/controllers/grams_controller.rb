f3b5897b1909a65997aade8abd904d44a3e6eabe
Teresa McClintic <tmcclintic@gmail.com>
2018-03-24 22:29:48 +0000

    user authentication

---
+++ app/controllers/grams_controller.rb
@@ -0,0 +1,29 @@
+class GramsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @gram = Gram.new
+  end
+
+  def create
+
+    @gram = current_user.grams.create(gram_params)
+    if @gram.valid?
+        redirect_to root_path
+      else
+        render :new, status: :unprocessable_entity
+    end
+    
+  end
+
+
+
+  def index
+  end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
+end
