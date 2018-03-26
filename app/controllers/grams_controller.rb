8de7c7e1544fc7a10a115a0933ab980d0d2c0a3e
Teresa McClintic <tmcclintic@gmail.com>
2018-03-26 00:01:10 +0000

    update-edit

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -5,6 +5,11 @@ class GramsController < ApplicationController
     @gram = Gram.new
   end
 
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  end
+
   def create
 
     @gram = current_user.grams.create(gram_params)
@@ -16,6 +21,22 @@ class GramsController < ApplicationController
     
   end
 
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+    @gram.update_attributes(gram_params)
+    if @gram.valid?
+      redirect_to root_path
+    else
+     return render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  end
+
 
 
   def index
@@ -25,5 +46,9 @@ class GramsController < ApplicationController
 
   def gram_params
     params.require(:gram).permit(:message)
+  end
+
+  def render_not_found
+    render plain: 'Not Found :(', status: :not_found
   end
 end
